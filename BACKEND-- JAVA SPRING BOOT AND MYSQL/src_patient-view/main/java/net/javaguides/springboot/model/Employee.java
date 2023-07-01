a11a9c8c3662d8570fd35d1a32b7ebe15330ff72
Shanmuga Priyan T <138189691+Shanu06github@users.noreply.github.com>
2023-07-01 11:56:53 +0530

    Add files via upload

---
+++ BACKEND-- JAVA SPRING BOOT AND MYSQL/src_patient-view/main/java/net/javaguides/springboot/model/Employee.java
@@ -0,0 +1,81 @@
+package net.javaguides.springboot.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="employee")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="specialist")
+	private String emsp;
+	
+	@Column(name="Age")
+	private String age;
+	
+	@Column(name="Phoneno")
+	private String phone;
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getEmsp() {
+		return emsp;
+	}
+	public void setEmsp(String emsp) {
+		this.emsp = emsp;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+
+}
